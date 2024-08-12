07ad9d8597ab76ab76242787f51cf2a208d7e474
KingBone <78333286+vhgamer2006@users.noreply.github.com>
2024-08-12 10:30:21 +0700

    pic.js

--- pic.js
+++ pic.js
@@ -99,7 +99,7 @@ var random_images_array = ["1363382418078.png",
 "patreon-5.png"
 ];
 
-path = 'pictures/qts/'; 
+path = 'https://catbox.moe/pictures/qts/'; 
 var num = Math.floor( Math.random() * random_images_array.length );
 var img = random_images_array[ num ];
 var imgStr = '<img src="' + path + img + '" alt="cute grill">';
